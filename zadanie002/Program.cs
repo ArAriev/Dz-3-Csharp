1972aba28ddc452efe8acb5eb964d75aa1d2402b
Aleksandr Onufriev <pskmp@yandex.ru>
2022-10-12 23:14:11 +0300

    dobavil zadanie002

---
+++ zadanie002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine());
+int B = 0;
+int rez = 0;
+
+while (A > 0)
+    {
+        B = A % 10;
+        rez = rez + B;
+        A = A / 10;
+    }
+Console.WriteLine($"Сумма введенного числа равна {rez}");
