e20eea53ce5a618ff390d0a3744ed984cb992e20
Aleksandr Onufriev <pskmp@yandex.ru>
2022-10-12 22:27:37 +0300

    dobavil zadanie001

---
+++ zadanie001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int A = 1;
+
+while (A <= N)
+    {
+        int B = A * A * A;
+        Console.WriteLine($"Кубом числа {A} будет {B}");
+        A++;
+    }
+    
