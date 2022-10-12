b9c24dc4fd0ba974deced5ce3be0e6dce2a69eaa
Aleksandr Onufriev <pskmp@yandex.ru>
2022-10-12 23:49:40 +0300

    dobavil zadanie004

---
+++ zadanie004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Enter Number: ");
+int N = int.Parse(Console.ReadLine());
+int a = 1;
+
+while (a <= N)
+    {
+        int b = a * a * a;
+        int c = b % 10;
+
+        if(c % 2 == 0)
+            {
+                Console.WriteLine($"Как сказал мой САНСЕЙ, что если вспомнить математику: то как то так получается {b}");
+            }
+
+            a++;
+    }
+
